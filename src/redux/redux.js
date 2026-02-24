5fb4262e6e5f2c74e43356f4a85bfa5891cebbe0
이준원 <ljw1205317@naver.com>
2026-02-24 15:59:05 +0900

    React 15일차 과제

---
+++ src/redux/redux.js
@@ -0,0 +1,39 @@
+import { combineReducers, legacy_createStore } from "redux";
+import data from "../assets/data";
+
+
+export const addTocart = (options, quantity, id) => {
+    return {
+        type : `addToCart`,
+        payload : {options, quantity, id}
+    }
+}
+
+export const removeFromCart = (id) => {
+    return {
+        type : `removeFromCart`,
+        payload : {id}
+    }
+}
+
+
+const cartReducer = (state = [] , action) => {
+    switch (action.type) {
+        case `addToCart`:
+            return [...state, action.payload]
+        case `removeFromCart`:
+            return state.filter(el => el.id !== action.payload.id)
+        default:
+            return state
+    }
+
+}
+
+const menuReducer = (state = data.menu , action) => {
+    return state
+}
+
+const rootReducer = combineReducers({cartReducer , menuReducer })
+
+export const store = legacy_createStore(rootReducer)
+
